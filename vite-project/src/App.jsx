9bd28b889bf76cbcd74d4d78a608deebec535f9d
valenti94br <valenti94br@gmail.com>
2023-05-03 13:03:57 +0200

    inicio proyecto

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App() {
+
+return (
+
+<div className="App">
+
+<Header />
+
+<ProfileDescription />
+
+<Stories />
+
+<PostList />
+
+</div>
+
+);
+
+}
+
+export default App
